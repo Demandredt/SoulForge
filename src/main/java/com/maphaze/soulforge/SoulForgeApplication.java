82a4c756b93877530c4cb1647efcd04f86c4899f
Demandredt <1827912332@qq.com>
2025-03-05 15:21:05 +0800

    Initialization

---
+++ src/main/java/com/maphaze/soulforge/SoulForgeApplication.java
@@ -0,0 +1,13 @@
+package com.maphaze.soulforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoulForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maphaze/soulforge/SoulForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maphaze.soulforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
